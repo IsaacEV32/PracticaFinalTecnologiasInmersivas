8224e55e0ea24e98be4488fa84cd32f4e90bf7a1
Isaac Escudero <isaac_escudero@euneiz.com>
2026-03-10 10:43:44 +0100

    El tanque puede disparar balas

---
+++ PracticaFinalTecnologiasInmersivas_Isaac/Assets/IsaacEscudero/Scripts/Balas.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+public class Balas : MonoBehaviour
+{
+    [SerializeField] float bulletLive;
+    bool chronometerOn = false;
+    private void Awake()
+    {
+        this.gameObject.SetActive(true);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!chronometerOn)
+        {
+            bulletLive -= Time.deltaTime;
+            StartCoroutine(TimeBullet());
+        }
+        if (bulletLive <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    IEnumerator TimeBullet()
+    {
+        chronometerOn = true;
+        yield return new WaitForSeconds(0.1f);
+        chronometerOn = false;
+    }
+}
--- PracticaFinalTecnologiasInmersivas_Isaac/Assets/IsaacEscudero/Scripts/Disparo.cs
+++ PracticaFinalTecnologiasInmersivas_Isaac/Assets/IsaacEscudero/Scripts/Disparo.cs
@@ -1,16 +1,21 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Disparo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform puntoDeMira;
+    [SerializeField] float bulletSpeed;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject b = Instantiate(bullet, puntoDeMira.position, Quaternion.identity);
+            b.GetComponent<Rigidbody>().AddForce(puntoDeMira.transform.forward * bulletSpeed, ForceMode.Impulse);
+        }
+       
     }
 }
